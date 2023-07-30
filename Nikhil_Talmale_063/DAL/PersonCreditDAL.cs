ce02349cad8e7f96a24d3e53607fb484d0c9e1f2
Nikhil <ntalmale07@gmail.com>
2023-07-30 13:53:50 +0530

    edit bookman

---
+++ Nikhil_Talmale_063/DAL/PersonCreditDAL.cs
@@ -0,0 +1,101 @@
+﻿using MySqlConnector;
+using Nikhil_Talmale_063.Models;
+
+namespace Nikhil_Talmale_063.DAL
+{
+    public class PersonCreditDAL
+    {
+
+        static string constr = @"server=localhost;userid=root;password=12345;database=creditcarddb";
+
+
+        public static List<Person> GetListOfPersons()
+        {
+            List<Person> list = new List<Person>();
+
+
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                con.Open();
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+
+                    cmd.Connection = con;
+                    cmd.CommandText = "Select  from Person ;";
+                    MySqlDataReader rd = cmd.ExecuteReader();
+                    Console.WriteLine("\n\n");
+                    while (rd.Read())
+                    {
+                        Person b = new Person()
+                        {
+                            Pid = rd.GetInt32(0),
+                            Fname = rd.GetString(1),
+                            Lname = rd.GetString(1),
+                        };
+
+                        list.Add(b);
+
+                    }
+
+                    con.Close();
+                }
+                return list;
+
+            }
+        }
+
+        public static Person GetData(int id)
+        {
+            Person b = new Person();
+            
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                con.Open();
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+
+                    cmd.Connection = con;
+                    
+                    cmd.CommandText = "select p.pid, p.fname,p.lname, c.cardno,c.expirydate,c.bankname, from person p ,creditcard c where p.pid = @id;";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    MySqlDataReader rd = cmd.ExecuteReader();
+
+                    Console.WriteLine("\n\n");
+                    while (rd.Read())
+                    {
+
+
+                        b.Pid = rd.GetInt32(0);
+                        b.Fname = rd.GetString(1);
+                        b.Lname = rd.GetString(2);
+                          
+                    
+                    }
+
+                    con.Close();
+                }
+                return b;
+
+            }
+        }
+
+
+
+
+
+
+    }
+    
+    
+
+
+
+
+
+
+
+
+      
+
+}
---
+++ Nikhil_Talmale_063/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace Nikhil_Talmale_063.Models
+{
+    public class Person
+    {
+        public int Pid { get; set; } 
+        public string Fname { get; set; }
+
+        public string Lname { get; set; }
+
+
+        public Creditcard c1 { get; set; }
+
+        public Person(int pid, string fname, string lname, Creditcard c1)
+        {
+            Pid = pid;
+            Fname = fname;
+            Lname = lname;
+            this.c1 = c1;
+        }
+
+        public Person()
+        {
+        }
+    }
+}
